27bf25c7800a2ca90a6260166e0760cd94261e5e
root <root@ip-172-31-9-236.ec2.internal>
2022-11-04 11:33:15 +0000

    new files added

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(konstpavlov.pp.ua www.konstpavlov.pp.ua)
+domains=(dockertask.club www.dockertask.club)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="k.mcfellow@gmail.com" # Adding a valid address is strongly recommended
